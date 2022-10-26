f8cc2cdbd77a656619644fab1d5393d6471eb922
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2022-10-25 21:09:40 -0300

    Dependencia native-stack y app con los navigation container, para agregar mas screens

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUp from "./src/screen/SignUp/SignUp"
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SignUp(form)"
+          component={SignUp}
+          options={{ title: 'Formulario de ingreso' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
