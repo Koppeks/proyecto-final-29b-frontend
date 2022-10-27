7bb7335278119e1eee3baf1465ca21908632337b
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-10-27 16:45:07 -0500

    navitationBar y Card integrada a Home

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
-import Home from "./src/screen/Home/Home";
-import HomeCardsProfessional from "./src/screen/Home/HomeCardsProfessional";
-import EditProfession from "./src/screen/Home/EditProfession";
+
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigationBar from "./src/components/NavigationBar/NavigationBar";
 
-const Stack = createNativeStackNavigator();
 
-export default function App() {
+
+export default function App()
+{
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Home Cards Professional"
-          component={HomeCardsProfessional}
-        />
-        <Stack.Screen
-          name="Edit Professional Profile"
-          component={EditProfession}
-        />
-      </Stack.Navigator>
+      <NavigationBar />
     </NavigationContainer>
   );
 }
