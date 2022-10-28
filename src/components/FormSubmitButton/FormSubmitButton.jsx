8c5fed34c3afafe706b2391e4bf5e99bc8b07ba7
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-10-28 17:19:52 -0500

    Fix filters image tag y correccion de card

--- src/components/FormSubmitButton/FormSubmitButton.jsx
+++ src/components/FormSubmitButton/FormSubmitButton.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
-const FormSubmitButton = props =>{
+const FormSubmitButton = props =>
+{
 
-  const {title, onPress, submitting, error} = props
+  const { title, onPress, submitting, error } = props
 
   const backCol = submitting ? 'rgba:(27,27,51,0.4)' : 'rgba:(27,27,51,1)'
-  return(
+  return (
     <>
       {
-        Object.keys(error).length > 0 
-        ?
-        <TouchableOpacity disabled={true} onPress={submitting ? null : onPress} style={{backCol}}>
-          <Text>{title}</Text>
-        </TouchableOpacity> 
-        : 
-        <TouchableOpacity onPress={submitting ? null : onPress} style={{backCol}}>
-          <Text>{title}</Text>
-        </TouchableOpacity>
+        Object.keys(error).length > 0
+          ?
+          <TouchableOpacity disabled={true} onPress={submitting ? null : onPress} style={{ backCol }}>
+            <Text>{title}</Text>
+          </TouchableOpacity>
+          :
+          <TouchableOpacity onPress={submitting ? null : onPress} style={{ backCol }}>
+            <Text>{title}</Text>
+          </TouchableOpacity>
       }
     </>
   )
--- src/components/button/ButtonNav.jsx
+++ src/components/button/ButtonNav.jsx
@@ -5,18 +5,19 @@ import tw from "twrnc"
 import filtro from '../../images/filtro.png'
 import { Image } from 'react-native-elements'
 
-export default function ButtonNav({navigation}) {
+export default function ButtonNav({ navigation })
+{
   return (
     <TouchableOpacity
 
-    onPress={() =>
-      navigation.navigate("Filtros", {
-        name: "Filtros",
-      })
-    }
-    style={tw`w-8 h-8 mr-5`}
-  >
-    <Image source={filtro} style={tw`w-8 h-8 mr-3 mt-3`} > </Image>
-  </TouchableOpacity>
+      onPress={() =>
+        navigation.navigate("Filtros", {
+          name: "Filtros",
+        })
+      }
+      style={tw`w-8 h-8 mr-5`}
+    >
+      <Image source={filtro} style={tw`w-8 h-8 mr-3 mt-3`} />
+    </TouchableOpacity>
   )
 }
