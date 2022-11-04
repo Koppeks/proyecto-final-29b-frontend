ef48cf29db35b2d9fafc21a88a1962b8a9f7df9c
graciela nieto <graciela.nieto.ayala@gmail.com>
2022-11-04 18:19:30 -0500

    jobform con selecto funcional

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,38 +1,45 @@
 import axios from "axios";
 import { setAuth } from "../reducers/authSlice";
 import { allCategories } from "../reducers/categoriesSlice";
-import {
+import
+{
   getProfessional,
   getProfessionalId,
   searchProfessionalName,
 } from "../reducers/profetionalSlice";
+const apikey = "1f5dbe34-3f44-4ec9-9d4b-078362fd7eb3";
 
 // get de profesionales
-export const getPro = () => async (dispatch) => {
+export const getPro = () => async (dispatch) =>
+{
   axios
     .get("https://proyecto-final-29b-backend-production.up.railway.app/professional")
     .then((res) => dispatch(getProfessional(res.data)))
     .catch((e) => console.log(e));
 };
 
-export const getProId = (id) => async (dispatch) => {
+export const getProId = (id) => async (dispatch) =>
+{
   axios
     .get("https://proyecto-final-29b-backend-production.up.railway.app/professional/id/" + id)
     .then((res) => dispatch(getProfessionalId(res.data)))
     .catch((e) => console.log(e));
 };
 
-export const getProName = (name) => async (dispatch) => {
+export const getProName = (name) => async (dispatch) =>
+{
   axios
     .get("https://proyecto-final-29b-backend-production.up.railway.app/professional" + name)
-    .then((res) => {
+    .then((res) =>
+    {
       dispatch(searchProfessionalName(res.data));
     })
     .catch((e) => console.log(e));
 };
 
 //post de profesionales
-export const postPro = (data) => async () => {
+export const postPro = (data) => async () =>
+{
   await axios({
     method: "POST",
     url: "https://proyecto-final-29b-backend-production.up.railway.app/professional",
@@ -42,21 +49,21 @@ export const postPro = (data) => async () => {
 
 //Hacer el filtrado de trabajos de expertos disponibles por categoria
 
-export const getCategories = () => async (dispatch) => {
+export const getCategories = () => async (dispatch) =>
+{
   axios
-    .get("https://proyecto-final-29b-backend-production.up.railway.app/professional")
+    .get(`https://proyecto-final-29b-backend-production.up.railway.app/occupation?apikey=${apikey}`)
     .then((res) => dispatch(allCategories(res.data)))
     .catch((e) => console.log(e));
 };
 
-
-
-export const logIn = (data) => async (dispatch) => {
+export const logIn = (data) => async (dispatch) =>
+{
   console.log(data)
 
-  const result={
-    data:{
-      jwt:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  const result = {
+    data: {
+      jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     }
   }
   // const result= await axios({
