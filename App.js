54094577b152450c4829c587979dc850735cd8a8
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2022-11-05 15:37:47 -0300

    Formulario de registro con fechas de nacimiento(falta validar y otros detalles)

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import {store,persistor} from "./src/redux/store";
 import { Provider } from "react-redux";
 import LandingPage from "./src/screen/LandinPage/LandingPage";
 import Login from "./src/screen/Login/Login";
-import CheckIn from "./src/screen/CheckIn/CheckIn";
 import { createStackNavigator } from "@react-navigation/stack";
 import MainContainer from "./src/containers/MainContainer/MainContainer";
 import { PersistGate } from "redux-persist/integration/react";
