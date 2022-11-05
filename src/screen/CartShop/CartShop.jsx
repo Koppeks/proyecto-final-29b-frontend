1216f6446d177798aceebf6f8c4e45f1f91435b1
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2022-11-05 19:32:14 -0300

    Estructura del carrito

--- src/screen/CartShop/CartShop.jsx
+++ src/screen/CartShop/CartShop.jsx
@@ -1,17 +1,28 @@
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { Header } from "react-native-elements/dist/header/Header";
+import tw from "twrnc"
+import GoBack from "../../components/button/GoBack"
 
-const CartShop = () =>
-{
-    return (
+const CartShop = ({navigation}) => {
 
-
-        <View >
-            <Text>CartShop</Text>
-        </View>
-
-
-
-    )
-}
+  return (
+    <View>
+			<Header
+				containerStyle={{
+					backgroundColor:'#f8f8f8',
+				}}
+				leftComponent={<GoBack navigation={navigation} title={"Ir atras"}/>}
+				centerComponent={{ text: 'Carrito', style: { color: '#333'} }}
+			/>
+			<View style={tw`flex`}>
+				<Text style={tw`text-5 text-center`}>Lista de compra</Text>
+				<View>
+					
+				</View>
+			</View>
+    </View>
+  );
+};
 
 export default CartShop;
