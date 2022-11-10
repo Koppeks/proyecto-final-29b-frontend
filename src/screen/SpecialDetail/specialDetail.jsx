33e7356fe93406fa35eb01fb7c2a28bfa01aec26
Daniel Gonzalez <daniel99gs.0@gmail.com>
2022-11-10 16:27:26 -0300

    listado de especialidades con filtro

---
+++ src/screen/SpecialDetail/specialDetail.jsx
@@ -0,0 +1,21 @@
+import { View, Text, ScrollView,Image } from 'react-native';
+import tw from "twrnc";
+;
+
+const SpecialDetail = () =>
+{
+    return (
+
+
+        <ScrollView >
+
+            Hola
+
+        </ScrollView>
+
+
+
+    )
+}
+
+export default SpecialDetail;
