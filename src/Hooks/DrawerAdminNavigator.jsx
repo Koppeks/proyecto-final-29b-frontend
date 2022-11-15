c9fb88b5d5d007f6f468e0cad64818dfe592979d
Alex <alexballen53@gmail.com>
2022-11-15 16:57:26 -0500

    Info Admin

--- src/Hooks/DrawerAdminNavigator.jsx
+++ src/Hooks/DrawerAdminNavigator.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeAdm from "../containers/ContainerHome/HomeAdm";
-
 import HomeNavigation from "./HomeNavigation";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerAdminNavigator = () => {
+  const { informationToken } = useSelector((state) => state.informationToken);
+  console.log(informationToken);
+  const name = informationToken?.fullName;
+  console.log("name", name);
+
+  const [isAdmin, setIsAdmin] = useState(false);
+
   return (
     <Drawer.Navigator useLegacyImplementation={true}>
-      <Drawer.Screen name="Home" component={HomeNavigation} />
-      <Drawer.Screen name="HomeAdm" component={HomeAdm} />
+      {informationToken.role === 9 ? (
+        <>
+          <Drawer.Screen name="Home" component={HomeNavigation} />
+          <Drawer.Screen name="HomeAdm" component={HomeAdm} />
+        </>
+      ) : (
+        <Drawer.Screen name="Home" component={HomeNavigation} />
+      )}
     </Drawer.Navigator>
   );
 };
